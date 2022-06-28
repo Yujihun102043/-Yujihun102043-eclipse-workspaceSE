20e58f727b1ab4a120d5777a47473a1e799af8e4
YuJiHun <dnjsxn01@naver.com>
2022-06-28 11:55:51 +0900

    ,,

---
+++ 97.JAVA_APPLICATION_ARCHITECTURE/src/com/itwill/address/AddressService.java
@@ -0,0 +1,10 @@
+package com.itwill.address;
+/*
+ * - 주소록(address) 관리 비즈니스로직(업무)를 수행하는 클래스
+ * - GUI(스윙, 서블릿, JSP)에서 직접 접근(메소드 호출)하는 클래스
+ * - AddressDao객체를 이용해서 데이터베이스에 접근하는 클래스
+ */
+public class AddressService {
+	
+
+}
