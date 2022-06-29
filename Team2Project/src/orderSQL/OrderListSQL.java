fffa98955df08d1dfeb3d7259148f97e23205a0a
YuJiHun <dnjsxn01@naver.com>
2022-06-29 20:49:15 +0900

    ..

---
+++ Team2Project/src/orderSQL/OrderListSQL.java
@@ -0,0 +1,14 @@
+package orderSQL;
+
+public class OrderListSQL {
+	public static final String ORDERLIST_INSERT
+	="insert into OrderList values(ORDERDETAIL_NO_SEQ,?,?,?,?)";
+	public static final String ORDERLIST_UPDATE
+	="update OrderList set ORDERDETAIL_NO=?,ORDER_NO=?,ORDER_QTY=?,ORDER_STMT=?,PRODUCT_NO=?";
+	public static final String ORDERLIST_DELETE
+	="delete from OrderList where ORDERDETAIL_NO=?";
+	public static final String ORDERLIST_SELECT_BY_NO
+	="select * from OrderList where ORDERDETAIL_NO=?";
+	public static final String ORDERLIST_SELECT_ALL
+	="select * from OrderList";
+}
---
+++ Team2Project/src/orderSQL/cartSQL.java
@@ -0,0 +1,14 @@
+package orderSQL;
+
+public class cartSQL {
+	public static final String CART_INSERT
+	="insert into cart values(?,cart_cart_no_seq.nextval,?,?)";
+	public static final String CART_UPDATE
+	="update cart set product_no = ?, member_no = ?, cart_qty = ? where cart_no=?";
+	public static final String CART_DELETE
+	="delete from cart where cart_no=?";
+	public static final String CART_SELECT_BY_NO
+	="select * from cart where cart_no=?";
+	public static final String CART_SELECT_ALL
+	="select * from cart";
+}
